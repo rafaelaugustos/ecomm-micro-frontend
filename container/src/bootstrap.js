a39681e1192bbae02329fd4c3bbba0b2f892d5b9
Rafael Augusto <rafaelaugusto.developer@gmail.com>
2021-04-18 19:55:05 -0300

    refactor: cart

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,5 +1,6 @@
-import { mount } from "products/ProductsIndex";
-import "cart/CartShow";
+import { mount as productsMount } from "products/ProductsIndex";
+import { mount as cartMount } from "cart/CartShow";
 
 console.log("container");
-mount(document.querySelector("#my-products"));
+productsMount(document.querySelector("#my-products"));
+cartMount(document.querySelector("#my-cart"));
